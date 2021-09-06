c03c8b139056277295d69e3b70475385e0ee9410
Tássila <tassilasantos2018@gmail.com>
2021-09-06 14:42:56 -0300

    api

---
+++ Área de Trabalho/c#/API/API/Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace API
+{
+    public abstract class EntidadeBase
+    {
+          public int Id { get; protected set; }
+        
+    }
+}
---
+++ Área de Trabalho/c#/API/API/interfaces/IRepositorios.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace API.Interfaces
+{
+    public interface IRepositorios<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);        
+        void Insere(T entidade);        
+        void Exclui(int id);        
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+         
+    }
+}
